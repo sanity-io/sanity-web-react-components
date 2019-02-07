a6874ee1e55cb96acbe103e1e9262702158cffe6
Kristoffer Sivertsen <kristoffer@sanity.io>
2019-02-07 13:20:47 +0100

    CSS module on button

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
+import * as styles from './Button.module.css'
 
 interface BaseProps {
   children: any
   disabled?: boolean
   onClick?: () => void
-  size?: 'normal' | 'large'
+  size?: 'normal' | 'large' | 'small'
 }
 
 interface LinkProps extends BaseProps {
@@ -23,10 +24,12 @@ const noop = () => void 0
 const Button = (props: Props) => {
   const { children, onClick, disabled } = props
   const commonProps = {
-    className: `sanity-button`,
+    className: styles.root,
     onClick: !disabled ? onClick : noop,
   }
-  if (props.size === 'large') commonProps.className += ` sanity-button--large`
+  if (props.size === 'large') commonProps.className += styles.large
+  if (props.size === 'small') commonProps.className += styles.small
+
   if (props.type === 'link') {
     return (
       <a {...commonProps} href={props.href}>
