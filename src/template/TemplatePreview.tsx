dd6c0b11d3e3dc9672e2fb40244a2978eed2b817
Marius Lundgård <studio@mariuslundgard.com>
2019-02-27 15:27:04 +0100

    Update template component

--- src/template/TemplatePreview.tsx
+++ src/template/TemplatePreview.tsx
@@ -4,13 +4,21 @@ import * as styles from './TemplatePreview.module.css'
 
 interface Props {
   isSelected?: boolean
+  onSelect: () => void
   subtitle: string
   title: string
 }
 
 function TemplatePreview(props: Props) {
   return (
-    <a className={props.isSelected ? styles.selected : styles.root} href="">
+    <a
+      className={props.isSelected ? styles.selected : styles.root}
+      href=""
+      onClick={evt => {
+        evt.preventDefault()
+        props.onSelect()
+      }}
+    >
       <div className={styles.image} />
       <div className={styles.text}>
         <div className={styles.title}>{props.title}</div>
