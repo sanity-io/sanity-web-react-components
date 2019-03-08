8e2017dbc34aa1a79ca567283edcea2a5e09ee57
Marius Lundgård <studio@mariuslundgard.com>
2019-03-08 16:07:19 +0100

    Fix spinner size

--- src/spinner/Spinner.tsx
+++ src/spinner/Spinner.tsx
@@ -5,7 +5,7 @@ import * as styles from './Spinner.module.css'
 function Spinner() {
   return (
     <svg className={styles.root} viewBox="0 0 40 40">
-      <circle cx="20" cy="20" r="18.5" />
+      <circle cx="20" cy="20" r="19.5" />
     </svg>
   )
 }
